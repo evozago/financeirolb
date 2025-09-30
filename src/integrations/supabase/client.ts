d73f209f3ab3a8496d63ac2326b37a9983ecf0fa
ESTEVAO ANDRADE ZAGO <evozago@gmail.com>
2025-09-30 09:17:29 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,13 +3,14 @@
 // Cliente do Supabase pronto para uso no browser (pode ficar público).
 // OBS: NUNCA exponha a service_role no frontend.
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = 'https://mnxemxgcucfuoedqkygw.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ueGVteGdjdWNmdW9lZHFreWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4OTY5MTYsImV4cCI6MjA2OTQ3MjkxNn0.JeDMKgnwRcK71KOIun8txqFFBWEHSKdPzIF8Qm9tw1o'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!  auth: {
     persistSession: true,
     autoRefreshToken: true,
   },
