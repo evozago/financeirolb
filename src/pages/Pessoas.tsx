76681b0f3f607ec8dedd506b7f99e9797dccdafa
ESTEVAO ANDRADE ZAGO <evozago@gmail.com>
2025-10-01 13:38:09 -0300

    Update Pessoas.tsx

--- src/pages/Pessoas.tsx
+++ src/pages/Pessoas.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { ArrowLeft } from "lucide-react";
